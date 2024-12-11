562e5b85bbd32ee883572debe56642f36bfca1fd
riya0007-bit <riya.sharma@quantique.ai>
2024-12-11 15:42:51 +0530

    home page tab created

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import {faEnvelope, faPhone, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faEnvelope, faPhone, faArrowRight);
+  }
 }
