758f6a6e63f90406ea4072d06a83a9fed7f37cf4
pinky <pinky@quantique.ai>
2024-12-11 18:32:56 +0530

    stapper

--- src/app/component/motor/vehicle-selection/vehicle-selection.component.ts
+++ src/app/component/motor/vehicle-selection/vehicle-selection.component.ts
@@ -2,11 +2,12 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SharedService } from '../shared.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-vehicle-selection',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [CommonModule,FontAwesomeModule],
   templateUrl: './vehicle-selection.component.html',
   styleUrl: './vehicle-selection.component.scss'
 })
@@ -14,7 +15,29 @@ export class VehicleSelectionComponent {
   constructor(private router: Router,private _sharedService: SharedService) { }  // Inject the Router into your constructor
   @Output() stepChange = new EventEmitter<void>();
 
-  goToNextStep() {
+  cards = [
+    {
+      type: 'twoWheeler',
+      icon: '/icons/bike.svg',
+      title: 'Two Wheeler',
+      description: 'Motorcycles or Scooters; Secure rides with 2-wheeler insurance: peace of mind, financial protection, hassle-free claims.'
+    },
+    {
+      type: 'fourWheeler',
+      icon: '/icons/car.svg',
+      title: 'Four Wheeler',
+      description: 'Cars; Four-wheeler insurance ensures protection: financial security, accident coverage, peace of mind on the road.'
+    },
+    {
+      type: 'commercial',
+      icon: '/icons/truck.svg',
+      title: 'Commercial',
+      description: 'Taxi, Truck, Bus, PCCV, Three Wheelers like Van, Tempo, Auto, Miscellaneous; Get business protection, liability coverage, financial security.'
+    }
+  ];
+
+
+  goToNextStep(type:string) {
     console.log(1,'click')
     this._sharedService.emitChange("Data from child"); // Emit the next step index
     this.router.navigate(['/pages/motor/two-wheeler/motor-detail'])
