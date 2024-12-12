c2d0bbfcd3ebfbeda7f22bf88f00cac7db27bf46
pinky <pinky@quantique.ai>
2024-12-12 11:02:38 +0530

    motor details

--- src/app/component/motor/two-wheeler/motor-detail/motor-detail.component.ts
+++ src/app/component/motor/two-wheeler/motor-detail/motor-detail.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchbarComponent } from '../../../../dynamic/searchbar/searchbar.component';
 
 @Component({
   selector: 'app-motor-detail',
   standalone: true,
-  imports: [],
+  imports: [SearchbarComponent],
   templateUrl: './motor-detail.component.html',
   styleUrl: './motor-detail.component.css'
 })
---
+++ src/app/dynamic/searchbar/searchbar.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-searchbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './searchbar.component.html',
+  styleUrl: './searchbar.component.css'
+})
+export class SearchbarComponent {
+
+}
