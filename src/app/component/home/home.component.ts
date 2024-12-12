951cd0753fdd6ce73ce62a9c0cf28b77b6f3924c
riya0007-bit <riya.sharma@quantique.ai>
2024-12-12 17:11:51 +0530

    home page completion

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,21 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import {faEnvelope, faPhone, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  constructor(library: FaIconLibrary,private router: Router) {
-    library.addIcons(faEnvelope, faPhone, faArrowRight);
+  constructor(private router: Router) {
   }
   motor() {
   this.router.navigate(['/pages/motor/vehicleselection'])
+  }
+
+  faqs =[
+    {
+      question: 'Question 01',
+      answer: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English.',
+      isOpen: true,
+    },
+    {
+      question: 'Question 02',
+      answer: 'Answer for question 2 goes here.',
+      isOpen: false,
+    },
+    {
+      question: 'Question 03',
+      answer: 'Answer for question 2 goes here.',
+      isOpen: false,
+    },
+    {
+      question: 'Question 04',
+      answer: 'Answer for question 2 goes here.',
+      isOpen: false,
+    },
+  ];
+  toggleFAQ(index: number){
+    this.faqs[index].isOpen = !this.faqs[index].isOpen;
   }
 }
