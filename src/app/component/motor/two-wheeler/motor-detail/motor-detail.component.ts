5eda7be9e9acf6836505f20b849eab38e67d95fa
KajalShuklaQuan <kajal.shukla@quantique.ai>
2024-12-13 12:41:42 +0530

    motor-detail

--- src/app/component/motor/two-wheeler/motor-detail/motor-detail.component.ts
+++ src/app/component/motor/two-wheeler/motor-detail/motor-detail.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { SearchbarComponent } from '../../../../dynamic/searchbar/searchbar.component';
-import { DynamicFormComponent } from '../../../../dynamic/dynamic-form/dynamic-form.component';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SharedService } from '../../shared.service';
+import { SearchbarComponent } from '../../../../dynamic/searchbar/searchbar.component';
+import { CommonModule } from '@angular/common';
+import { DynamicFormComponent } from '../../../../dynamic/dynamic-form/dynamic-form.component';
 
 @Component({
-  selector: 'app-motor-detail',
+  selector: 'app-motor-deta il',
   standalone: true,
-  imports: [SearchbarComponent, DynamicFormComponent],
+  imports: [SearchbarComponent, DynamicFormComponent,CommonModule],
   templateUrl: './motor-detail.component.html',
   styleUrls: ['./motor-detail.component.scss'] // Corrected styleUrls
 })
 
-export class MotorDetailComponent implements OnInit {
+export class MotorDetailComponent implements OnInit, AfterViewInit {
   currentStep: number = 1;
+  selectedPolicy: string = '';  // Track the selected policy (new or renew)
 
   constructor(private router: Router, private _sharedService: SharedService) {}
 
@@ -50,15 +52,24 @@ export class MotorDetailComponent implements OnInit {
     },
   ];
 
-  ngOnInit() {
- 
-  }
-  ngAfterViewInit() {
+  ngOnInit() {}
 
+  ngAfterViewInit() {
     this._sharedService.emitChange(this.currentStep);
   }
   onFormSubmit(formData: any): void {
     console.log('Form submitted:', formData);
     // Perform actions with the submitted form data
   }
+
+  onRadioSelect(policyType: string) {
+    this.selectedPolicy = policyType;
+  }
+
+  onClickOutside() {
+    this.selectedPolicy = ''; // Reset when clicking outside
+  }
+  
 }
+
+
