1bf66ddb5410872d637232a93578ad823529bee4
riya0007-bit <riya.sharma@quantique.ai>
2024-12-13 18:58:33 +0530

    select-plan in process

--- src/app/component/motor/two-wheeler/select-plan/select-plan.component.ts
+++ src/app/component/motor/two-wheeler/select-plan/select-plan.component.ts
@@ -1,12 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-select-plan',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './select-plan.component.html',
   styleUrl: './select-plan.component.css'
 })
 export class SelectPlanComponent {
+  minValue = 463239;
+  maxValue = 959898;
+  currentValue = this.minValue;
 
+  onSliderChange(){
+    console.log('Current IDV Value:', this.currentValue);
+  }
 }
