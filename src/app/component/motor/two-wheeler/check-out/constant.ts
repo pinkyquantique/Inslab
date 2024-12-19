e97cecd9d7fb9db9e9f91bacf0e0110484f1bbda
KajalShuklaQuan <kajal.shukla@quantique.ai>
2024-12-19 14:46:22 +0530

    checkout

--- src/app/component/motor/two-wheeler/check-out/constant.ts
+++ src/app/component/motor/two-wheeler/check-out/constant.ts
@@ -7,19 +7,50 @@ export const proposalData = {
     gst: 950001,
     totalPremium: '19,50,001',
     owner: {
-      insuredName: 'Neerav',
+      insuredName: 'NEERAV',
+      insuredAge:46,
+      previousPolicyNo:'484GTKMS48',
       insuredAadhaar: '2048497623656',
       insuredPan: 'FRUIBK96E4',
       nomineeName: 'Roa',
       nomineeRelation: 'Daughter',
-      insuredAddress: 'P-301, Galaxy Apartments, Malabar Hills, Mumbai, Maharashtra'
+      insuredAddress: 'P-301, Galaxy Apartments, Malabar Hills, Mumbai, Maharashtra',
+      GSTNo:'4875JYTGG58',
+      AppointeeName: 'NEERAVI',
+      AppointeeAge: '41',
+      AppointeeRelation: 'SPOUSE',
+      NomineeName:'ROA',
+      NomineeAge:14,
+      RelationWithNominee:'DAUGHTER',
+      InsuredAddress:'p-301, Galaxy apartments, starlight road, malabar hills',
+      city:'MUMBAI',
+      pincode:400055,
+      state:'MAHARASHTRA'
+
+
     },
     vehicle: {
-      make: 'Honda',
-      model: 'Activa',
+      make: 'HONDA',
+      model: 'ACTIVA',
       registrationNumber: 'MH09AP1122',
-      fuelType: 'Petrol',
-      registrationDate: '2023-10-25'
+      fuelType: 'PETROL',
+      varient:'DISC BRAKE ALLOY',
+      registrationDate: '2023-10-25',
+      cubicCapacity:'125 CC',
+      color:'PEARL AMAZING WHITE',
+      seatingCapacity:2,
+      BodyType:'SCOOTER',
+      Registerdate: '2024-05-15',
+      RTO:'MH01',
+      ElectricAccessories:'₹ 15,000',
+      EngineNo:'HNGA125ENG56789',
+      ManufacturingMonth:'MARCH',
+      VehicleIDv:'₹ 85,000',
+      GVW:'282 kg'
+
+
+
+    
     }
   };
   
